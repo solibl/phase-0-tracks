2c79d602bf05a7d6e1985bb0cd7edda53aa0c910
Brian Lee <brianleedongjun@gmail.com>
2017-02-17 11:57:45 -0800

    Finished Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,7 +10,45 @@ year = gets.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
 garlicbread = gets.chomp
 
+counter = 0
+until counter == 1
+  if garlicbread == "Yes"
+      counter = counter + 1
+  elsif garlicbread == "No"
+      counter = counter + 1
+  else
+      puts "Please answer the question as Yes or No"
+      garlicbread = gets.chomp
+  end
+end
+
+
 puts "Would you like to enroll in the company's health insurance? (Yes/No)"
 insurance = gets.chomp
 
-puts "#{name}, #{age}, #{year}, #{garlicbread}"
+counter = 0
+until counter == 1
+  if insurance == "Yes"
+      counter = counter + 1
+  elsif insurance == "No"
+      counter = counter + 1
+  else
+      puts "Please answer the question as Yes or No"
+      insurance = gets.chomp
+  end
+end
+
+# puts "#{name}, #{age}, #{year}, #{garlicbread}, #{insurance}"
+
+# Vampire Calculator
+if (name != "Drake Cula" && name != "Tu Fang" ) && (age == 2016 - year) && (garlicbread == "Yes" || insurance == "Yes")
+  puts "#{name} probably not a vampire"
+elsif (name != "Drake Cula" && name != "Tu Fang" ) && (age != 2016 - year) && (garlicbread == "No" || insurance == "No")
+  puts "#{name} probably a vampire"
+elsif (name != "Drake Cula" && name != "Tu Fang" ) && (age != 2016 - year) && garlicbread == "No" && insurance == "No"
+  puts "#{name} almost certainly a vampire"
+elsif name == "Drake Cula" || name == "Tu Fang"
+  puts "#{name} definitely a vampire"
+else
+  puts "Results inconclusive"
+end
